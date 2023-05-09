01b582c1b641a1dd20205904c4c825ab709131cd
Gabriel Iciarte <gabriel_viki@hotmail.com>
2023-05-09 18:17:37 -0300

    update to deploy

--- src/database/db.js
+++ src/database/db.js
@@ -2,33 +2,33 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 var mysql = require("mysql2/promise");
-//descomentar para conecxion remota
 
-// const host = process.env.HOST;
-// const user = process.env.USER;
-// const password = process.env.PASSWORD;
-// const database = process.env.DATABASE;
-
-// const connection = mysql.createPool({
-//   host: host,
-//   user: user,
-//   password: password,
-//   database: database,
-// });
-
-//conexion Local
-
-const host = "localhost";
-const user = "root";
-const password = "123456";
-const database = "airline";
+//deploy the database
+const host = process.env.HOST;
+const user = process.env.USER;
+const password = process.env.PASSWORD;
+const database = process.env.DATABASE;
 
 const connection = mysql.createPool({
   host: host,
   user: user,
   password: password,
   database: database,
-  port: "3306",
 });
+
+// Localhost
+// const host = process.env.HOST_LOCAL;
+// const user = process.env.USER_LOCAL;
+// const port = process.env.PORT_LOCAL;
+// const password = process.env.PASSWORD_LOCAL;
+// const database = process.env.DATABASE_LOCAL;
+
+// const connection = mysql.createPool({
+  // host: host,
+  // user: user,
+  // password: password,
+  // database: database,
+//   port: port,
+// });
 
 module.exports = connection;
