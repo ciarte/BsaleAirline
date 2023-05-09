ea373f2d89e5ad72eed0235c0764d4ac68e7c0b2
Gabriel Iciarte <gabriel_viki@hotmail.com>
2023-05-09 18:08:56 -0300

    update localhost

--- src/database/db.js
+++ src/database/db.js
@@ -1,18 +1,34 @@
-// const { Sequelize } = require('sequelize');
-require('dotenv').config();
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
 var mysql = require("mysql2/promise");
-const host = process.env.HOST;
-const user = process.env.USER;
-const password = process.env.PASSWORD;
-const database = process.env.DATABASE;
+//descomentar para conecxion remota
+
+// const host = process.env.HOST;
+// const user = process.env.USER;
+// const password = process.env.PASSWORD;
+// const database = process.env.DATABASE;
+
+// const connection = mysql.createPool({
+//   host: host,
+//   user: user,
+//   password: password,
+//   database: database,
+// });
+
+//conexion Local
+
+const host = "localhost";
+const user = "root";
+const password = "123456";
+const database = "airline";
 
 const connection = mysql.createPool({
   host: host,
   user: user,
   password: password,
   database: database,
+  port: "3306",
 });
-
 
 module.exports = connection;
