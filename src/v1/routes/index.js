32d0efa899b5ee716acf890c927b742135e29cfa
Gabriel Iciarte <gabriel_viki@hotmail.com>
2023-05-10 14:39:27 -0300

    comment route for get all seats

--- src/v1/routes/index.js
+++ src/v1/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { getAllFlights, getFlightsByID } = require("../../controllers/flightsController");
 
 router
-  .get("/", getAllFlights)
+  // .get("/", getAllFlights)
 
   .get("/:id/passengers", getFlightsByID);
 
